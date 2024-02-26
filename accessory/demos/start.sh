b7fccf68249b7a2ee4056832880ce45687dcfeb4
FanqingM <71324430+FanqingM@users.noreply.github.com>
2024-02-26 15:17:20 +0800

    Update start.sh

--- accessory/demos/start.sh
+++ accessory/demos/start.sh
@@ -1,3 +1,3 @@
-srun -p gvembodied --gres=gpu:1 --job-name=bf_Chart_debug --cpus-per-task 12 -n1 --ntasks-per-node=1 --quotatype=spot python -u single_turn_mm.py --llama_config params.json \
---tokenizer_path tokenizer.model \
---pretrained_path model_path $QUANT --llama_type llama_ens5
+torchrun --nproc-per-node=1 --master-port=12345 /root/cloud_disk2/LLaMA2-Accessory/accessory/demos/single_turn_mm.py --llama_config /root/cloud_disk2/LLaMA2-Accessory/params.json \
+--tokenizer_path /root/cloud_disk2/LLaMA2-Accessory/tokenizer.model \
+--pretrained_path /root/cloud_disk2/LLaMA2-Accessory/accessory/exps/finetune/mm/output/finetune/mm/chart_multitask_instruction_tuning_gpu8_ft_stock/epoch0 $QUANT --llama_type llama_ens5 > /root/cloud_disk2/LLaMA2-Accessory/accessory/demos/start1.log 2>&1 & \
